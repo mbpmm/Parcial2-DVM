12c20e99949ad2d82e180a54c4f82a30c677a3e2
mbpmm <marianomcr92@gmail.com>
2019-11-11 12:55:32 -0300

    Agregado balas y arma, arreglar muerte de enemigos

---
+++ DVM-Parcial2/Assets/Scripts/BulletController.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletController : MonoBehaviour
+{
+    public float speed;
+    public float lifeSpan;
+    private float timer;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        timer += Time.deltaTime;
+        transform.position += transform.forward * Time.deltaTime * speed;
+
+        if (timer >= lifeSpan)
+        {
+            gameObject.SetActive(false);
+            timer = 0;
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Zombie")
+        {
+            gameObject.SetActive(false);
+            other.gameObject.SetActive(false);
+            timer = 0;
+        }
+
+        if (other.gameObject.tag == "Vampire")
+        {
+            gameObject.SetActive(false);
+            other.gameObject.SetActive(false);
+            timer = 0;
+        }
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "Zombie")
+        {
+            gameObject.SetActive(false);
+            collision.gameObject.SetActive(false);
+            timer = 0;
+        }
+
+        if (collision.gameObject.tag == "Vampire")
+        {
+            gameObject.SetActive(false);
+            collision.gameObject.SetActive(false);
+            timer = 0;
+        }
+    }
+}
---
+++ DVM-Parcial2/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class PlayerController : MonoBehaviour
+{
+    public GameObject bulletEmitter;
+    public float nextFire;
+    public float fireRate;
+
+    public LayerMask rayCastLayer;
+    public float rayDistance;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        RaycastHit hit;
+        Debug.DrawRay(transform.position, Vector3.left * 20, Color.red);
+
+        if (Physics.Raycast(transform.position, transform.forward, out hit, rayDistance, rayCastLayer))
+        {
+            Debug.DrawRay(transform.position, transform.forward * hit.distance, Color.yellow);
+
+            string layerHitted = LayerMask.LayerToName(hit.transform.gameObject.layer);
+
+            switch (layerHitted)
+            {
+                case "Enemies":
+                    if (Time.time > nextFire)
+                    {
+                        nextFire = Time.time + fireRate;
+                        Shoot();
+                    }
+                    break;
+            }
+
+        }
+        
+    }
+
+    public void Shoot()
+    {
+        GameObject b = ObjectPool.instance.GetPooledObject("Bullet");
+        b.transform.position = bulletEmitter.transform.position;
+        b.transform.rotation = bulletEmitter.transform.rotation;
+    }
+}
